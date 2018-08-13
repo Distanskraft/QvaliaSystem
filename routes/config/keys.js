8256d51016b9cfe8af4f33efacff1224457ac349
subflow <33748960+subflow@users.noreply.github.com>
2018-08-13 21:24:06 +0200

    Delete keys.js

--- routes/config/keys.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  distanskraftToken: '0/e4cf6b246b525387a97b0657d7a339c9',
-  qvaliaCom: '168854438357379',
-  analysis: '172160552796217',
-  distanskraftSe: '542024449570027',
-  PersonalProjectsAdminDistanskraftSe: '498346170860'
-};
