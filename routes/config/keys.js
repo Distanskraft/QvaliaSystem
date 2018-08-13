b56b952305875770e0a042770b118c0a304c7f13
Jonas Olsson <jonas@lowtek.se>
2018-08-13 21:37:14 +0200

    added prod and dev keys

--- routes/config/keys.js
+++ routes/config/keys.js
@@ -1,7 +1,5 @@
-module.exports = {
-  distanskraftToken: '0/e4cf6b246b525387a97b0657d7a339c9',
-  qvaliaCom: '168854438357379',
-  analysis: '172160552796217',
-  distanskraftSe: '542024449570027',
-  PersonalProjectsAdminDistanskraftSe: '498346170860'
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
