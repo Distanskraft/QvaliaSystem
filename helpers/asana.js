96cb92d89c5048acba2d4863756120f479b41f1a
Jonas Olsson <jonas@lowtek.se>
2018-08-31 11:37:39 +0200

    nu jävlar

--- helpers/asana.js
+++ helpers/asana.js
@@ -13,6 +13,123 @@ const {
 // Connect to asana
 const client = asana.Client.create().useAccessToken(keys.distanskraftToken);
 
+// UPDATE CUSTOM FIELD BY NAME FUNCTION
+async function updateCustomFieldByName(taskId, cfName, cfValue) {
+  /* Add the below into the router.post section: 
+    const resp = await updateCustomFieldByName(
+    req.body.taskId,
+    req.body.cfName,
+    req.body.cfValue
+  );
+  //console.log('response', resp);
+  res.json(resp);
+  */
+
+  // Pull the task from Asana.
+  let task = await a.getTaskById(taskId);
+
+  if (task.toString() != 'Error: Invalid Request') {
+    // Storage for Custom Field Id.
+    let cfId = '';
+
+    // Object used to send the custom fields to the Asana API
+    let myCustom_field = {};
+
+    // Filter the custom fields by name, if no hit, return error.
+    return task.custom_fields.filter(function(field) {
+      if (field.name === cfName) {
+        if (field.type === 'text') {
+          // Set the custom field id
+          cfId = field.id;
+          // Set the custom field object id to the incomming value.
+          myCustom_field[cfId] = cfValue;
+          // Update the task accordingly
+          return client.tasks
+            .update(task.id, {
+              name: task.name,
+              custom_fields: myCustom_field
+            })
+            .then(response => {
+              return (
+                'Successfully updated custom field' +
+                cfName +
+                ' to value ' +
+                cfValue +
+                ' in task ' +
+                taskId +
+                '.\n' +
+                response
+              );
+            })
+            .catch(err => console.log(err.value.errors));
+        } else if (field.type === 'number') {
+          // Set the custom field id
+          cfId = field.id;
+          // Set the custom field object id to the incomming value.
+          myCustom_field[cfId] = cfValue;
+          // Update the task accordingly
+          return client.tasks
+            .update(task.id, {
+              name: task.name,
+              custom_fields: myCustom_field
+            })
+            .then(response => {
+              return (
+                'Successfully updated custom field' +
+                cfName +
+                ' to value ' +
+                cfValue +
+                ' in task ' +
+                taskId +
+                '.\n' +
+                response
+              );
+            })
+            .catch(err => console.log(err.value.errors));
+        } else if (field.type === 'enum') {
+          field.enum_options.forEach(enum_options => {
+            if (enum_options.name === cfValue) {
+              // Set the custom field id
+              cfId = field.id;
+              // Set the custom field object id to the id which matches the incomming value.
+              myCustom_field[cfId] = enum_options.id;
+              // Update the task accordingly
+              return client.tasks
+                .update(task.id, {
+                  name: task.name,
+                  custom_fields: myCustom_field
+                })
+                .then(response => {
+                  return (
+                    'Successfully updated custom field' +
+                    cfName +
+                    ' to value ' +
+                    cfValue +
+                    ' in task ' +
+                    taskId +
+                    '.\n' +
+                    response
+                  );
+                })
+                .catch(err => console.log(err.value.errors));
+            } else {
+              // TODO: Error handling not working correctly. Gotta fix that for later.
+              return 'ERROR 3. The requested custom field was found, but was enum and the requested update value was not in the enum list..';
+            }
+          });
+        }
+        // TODO: Error handling not working correctly. Gotta fix that for later.
+      } else {
+        return (
+          'ERROR 2. The requested custom field ' + cfName + ' was not found.'
+        );
+      }
+    });
+  } else {
+    return 'ERROR 1. No Asana task was found with ID: ' + taskId;
+  }
+}
+
 //Function to send post request to webhook -- zapier?
 function onServiceTaskCompletion(projectId, data) {
   const hookData = {
@@ -114,7 +231,12 @@ function subscribeToAsanaWebhooks(eventList, resourceId) {
                 if (_event.type == 'story') {
                   console.log('made it to MASTER case');
                   // Check for story text
-                  if (data.text.match(/SYSTEM COMMAND/i)) {
+                  if (data.text.match(/SYSTEM COMMAND to "Action here"/i)) {
+                    updateCustomFieldByName(
+                      _event.target.id,
+                      'ID Number',
+                      'snigel'
+                    );
                     console.log('yes');
                   }
                 }
@@ -124,7 +246,7 @@ function subscribeToAsanaWebhooks(eventList, resourceId) {
 
               case 'CREATEDCOMPLETED':
                 if (_event.type == 'story') {
-                  console.log('made it to service case');
+                  console.log('made it to CREATEDCOMPLETED case');
                   // Check for story text
                   if (
                     data.text.match(
