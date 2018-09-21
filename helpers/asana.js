43d1e4e7a23f5a9e399bd6106fb6285599e6c23d
a7n8xx12xp5ld <magnus.ostlund@leadxd.com>
2018-09-21 20:38:53 +0200

    test17

--- helpers/asana.js
+++ helpers/asana.js
@@ -268,29 +268,24 @@ function subscribeToAsanaWebhooks(eventList, resourceId) {
                       'MASTER CASE - STORY - ESTIMATED INVOICE VALUE'
                     );
                     // Pull the custom field info for "Estimated Invoice Value" as JSON
-                    let estimated_invoice_value_CF_JSON = data.target.custom_fields.filter(
+                    let EIV_JSON = data.target.custom_fields.filter(
                       response => {
                         return response.name === 'Estimated Invoice Value';
                       }
                     );
                     // Set the value for the estimated invoice value variable
-                    let estimated_invoice_value =
-                      estimated_invoice_value_CF_JSON[0].number_value;
-                    console.log(
-                      'estimated_invoice_value: ' + estimated_invoice_value
-                    );
+                    let EIV = EIV_JSON[0].number_value;
+                    console.log('estimated_invoice_value: ' + EIV);
 
                     // Pull the custom field info for "Verification Currency" (ENUM)
-                    let estimated_invoice_value_CF_Currency_JSON = data.target.custom_fields.filter(
+                    let EIV_curr_JSON = data.target.custom_fields.filter(
                       response => {
                         return response.name === 'Verification Currency';
                       }
                     );
-                    let estimated_invoice_value_currency =
-                      estimated_invoice_value_CF_Currency_JSON[0].number_value;
+                    let EIV_curr = EIV_curr_JSON[0].enum_value;
                     console.log(
-                      'estimated_invoice_value: ' +
-                        estimated_invoice_value_currency
+                      'estimated_invoice_value currency: ' + EIV_curr
                     );
 
                     let logString;
